610dda80d098d99954f6e9dfa4e7392602370e8d
Tamara <terratamara@yahoo.com>
2024-02-14 02:47:03 -0800

    Updated Greedy and FKnapsack

--- Project2/src/FractionalKnapsack/Greedy.java
+++ Project2/src/FractionalKnapsack/Greedy.java
@@ -1,59 +1,71 @@
 package FractionalKnapsack;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 
 public class Greedy {
 
-    public void run(int capacity, int[] weight, int[] value){
+    public void run(int capacity, int[] weight, int[] value) {
         //Int variable to hold our weight. Array to hold our items, double to hold our profit, and an array for ratios
         int length = weight.length;
-        itemArray[] items = new itemArray[length];
-        int[] ratio = new int[length];
+        FKnapsack order = new FKnapsack();
+        FKnapsack[] list = new FKnapsack[length];
         double profit = 0;
+        ArrayList<Integer> itemSelected = new ArrayList<>();
+        int capcityLeft = capacity;
 
-        int itemName = 0;
-        //For loops that calculates our ratios + inserts our weight, values, and ratios into an item array.
-        for(int index = 0; index < length; index++ ){
-            itemName += 1;
-            ratio[index] = value[index]/weight[index];
-            items[index] = new itemArray(itemName, weight[index], value[index], ratio[index]);
+        for (int index = 0; index < weight.length; index++) {
+
+            FKnapsack temp = new FKnapsack();
+            temp.setValue(value[index]);
+
+            double ratioTemp = value[index] / weight[index];
+            temp.setRatio(ratioTemp);
+
+            temp.setTitle(index);
+            temp.setWeight(weight[index]);
+
+            list[index] = temp;
 
         }
 
+
         //Sorts the items array in desecending order based on ratios. + Converts integers into doubles
-        Arrays.sort(items, Comparator.comparing((itemArray item) -> item.ratio).reversed());
+        for (int i = 0; i < list.length - 1; i++) {
+            if (list[i].getRatio() < list[i + 1].getRatio()) {
+                FKnapsack temp = new FKnapsack();
+                temp = list[i];
+                list[i] = list[i + 1];
+                list[i + 1] = temp;
+            }
 
+        }
 
-            //Iterates through our item arrays.
-            for (itemArray total : items) {
-                //If the capacity is greater or equal to our current weight we add the value and - the weight from capcity.
-                if (capacity >= total.weight) {
+        while (capcityLeft > 0) {
+            for (FKnapsack e : list) {
+                if (e.getWeight() <= capcityLeft && !itemSelected.contains(e.getTitle())) {
+                    profit += e.getValue();
+                    capcityLeft -= e.getWeight();
+                    itemSelected.add(e.getTitle());
 
-                    profit += total.value;
-                    capacity -= total.weight;
-
-                    System.out.println("Item " + total.itemName + " added.");
-                    System.out.println("Current Profit: " + profit);
-                    System.out.println("Current capacity: " + capacity + "\n");
-
+                } else {
+                    profit += capcityLeft * e.getRatio();
+                    capcityLeft = 0;
+                    itemSelected.add(e.getTitle());
                 }
-                //If the item's weight is too large for the weight capacity of the sack. We fraction the item added and break the loop.
-                else {
 
-                    profit += (total.ratio * capacity);
+                for (int i : itemSelected) {
+                    System.out.println("Item's Selected: ");
+                    System.out.print(i + ", \n");
 
-                    System.out.println("Fraction of Item " + total.itemName + " added.");
-                    System.out.println("Current capacity: " + capacity + "\n");
-                    break;
+
                 }
             }
+
+        }
 
         System.out.println("Total Profit = " + profit);
     }
 
-
 }
-
-
-
